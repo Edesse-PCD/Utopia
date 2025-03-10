b797fe1e3f6ae5e43efc9e28cfcc8be0469d7e44
Edesse PINCEDE <edesse.pincede@icloud.com>
2025-03-10 17:03:21 +0100

    Niveau 3 en cours de création Edesse

--- phaser_environnement_vide_tuto3/src/js/niveau3.js
+++ phaser_environnement_vide_tuto3/src/js/niveau3.js
@@ -7,13 +7,52 @@ export default class niveau3 extends Phaser.Scene {
         key: "niveau3" //  ici on précise le nom de la classe en tant qu'identifiant
       });
     }
-    preload() {}
+    preload() {
+     // chargement tuiles de jeu
+this.load.image("Phaser_tuilesdejeu", "src/assets/Assets.png");
+this.load.image("Phaser_tuilesdejeu", "src/assets/Oiseau.png");
+this.load.image("Phaser_tuilesdejeu", "src/assets/tuiles_de_jeu.png");
+// chargement de la carte
+this.load.tilemapTiledJSON("carte", "src/assets/map.json");  
+    }
   
     create() {
-      this.add.image(400, 300, "img_ciel");
-      this.groupe_plateformes = this.physics.add.staticGroup();
-      this.groupe_plateformes.create(200, 584, "img_plateforme");
-      this.groupe_plateformes.create(600, 584, "img_plateforme");
+     
+// chargement du jeu de tuiles
+const tileset = carteDuNiveau.addTilesetImage(
+          "tuiles_de_jeu",
+          "Phaser_tuilesdejeu"
+        );  
+
+        const calque_background = carteDuNiveau.createLayer(
+          "Background",
+          tileset
+        );
+
+// chargement du calque calque_background_2
+const calque_background_2 = carteDuNiveau.createLayer(
+          "Background_2",
+          tileset
+        );
+
+// chargement du calque calque_plateformes
+const calque_plateformes = carteDuNiveau.createLayer(
+          "Plateformes",
+          tileset
+        );  
+
+        calque_plateformes.setCollisionByProperty({ estSolide: true }); 
+        this.physics.add.collider(player, plateforme); 
+        const carteDuNiveau = this.add.tilemap("carte");
+        this.physics.world.setBounds(0, 0, 3200, 640);
+  //  ajout du champs de la caméra de taille identique à celle du monde
+  this.cameras.main.setBounds(0, 0, 3200, 640);
+  // ancrage de la caméra sur le joueur
+  this.cameras.main.startFollow(player); 
+  
+
+
+     
       this.porte_retour = this.physics.add.staticSprite(100, 550, "img_porte3");
       // ajout d'un texte distintcif  du niveau
       this.add.text(400, 100, "Vous êtes dans le niveau 3", {
@@ -40,8 +79,9 @@ export default class niveau3 extends Phaser.Scene {
         this.player.setVelocityX(0);
         this.player.anims.play("anim_face");
       }
-      if (this.clavier.up.isDown && this.player.body.touching.down) {
-        this.player.setVelocityY(-330);
+      if (clavier.up.isDown && player.body.blocked.down) {
+        player.setVelocityY(-200);
+      
       }
       if (Phaser.Input.Keyboard.JustDown(this.clavier.space) == true) {
         if (this.physics.overlap(this.player, this.porte_retour)) {
