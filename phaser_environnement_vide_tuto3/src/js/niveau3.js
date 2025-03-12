95c030386dab2382a69b38d6bc7a8371b89d5359
Noemie PHILIP <noemie.philip@epfedu.fr>
2025-03-12 16:27:55 +0100

    image niveau 2

--- phaser_environnement_vide_tuto3/src/js/niveau3.js
+++ phaser_environnement_vide_tuto3/src/js/niveau3.js
@@ -15,6 +15,8 @@ export default class niveau3 extends Phaser.Scene {
     this.load.image("tileset_image", "src/assets/victoire_image.png");
     this.load.image("tileset_bouton", "src/assets/bouton.png");
     this.load.image("bouton","src/assets/bouton.png")
+    this.load.image("imageNiveau3","src/assets/Niveau_3/imageNiveau3.png")
+
 
     
 
@@ -24,7 +26,34 @@ export default class niveau3 extends Phaser.Scene {
 
   create() {
     this.maxDistance = 700; // Distance maximale autorisée entre les joueurs
-
+    this.imageNiveau3 = this.add.image(400, 300, "imageNiveau3").setDepth(10).setScale(0.5);
+    this.boutonCommencer = this.add.image(
+      this.cameras.main.width - 250, // Position X en haut à droite
+      360, // Position Y en haut
+      "tileset_bouton" // Clé de ton image de bouton
+    ).setOrigin(0.5)
+    .setScrollFactor(0) // Rendre le bouton fixe par rapport à la caméra
+    .setInteractive().setScale(0.20).setDepth(11);
+    
+    // Ajouter le texte "Menu" par-dessus le bouton
+    this.texteCommencer = this.add.text(
+      this.boutonCommencer.x, // Position X centrée sur le bouton
+      this.boutonCommencer.y, // Position Y centrée sur le bouton
+      "Commencer",
+      {
+          font: "20px Arial",
+          fill: "#000",   // Texte en noir
+          align: "center"
+      }
+    ).setOrigin(0.5)
+    .setScrollFactor(0).setDepth(12); // Rendre le texte fixe par rapport à la caméra
+    
+    // Rendre le bouton cliquable
+    this.boutonCommencer.on("pointerdown", () => {
+      this.boutonCommencer.destroy();
+      this.imageNiveau3.destroy();
+      this.texteCommencer.destroy();
+        });
     // chargement du jeu de tuiles
     const carteDuNiveau = this.add.tilemap("carte");
     const tileset = carteDuNiveau.addTilesetImage(
@@ -73,10 +102,7 @@ export default class niveau3 extends Phaser.Scene {
 
 
     // ajout d'un texte distintcif  du niveau
-    this.add.text(400, 100, "Vous êtes dans le niveau 3", {
-      fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',
-      fontSize: "22pt"
-    });
+
 
     this.player = this.physics.add.sprite(350, 450, "img_dino");
     this.player.setBounce(0.2);
