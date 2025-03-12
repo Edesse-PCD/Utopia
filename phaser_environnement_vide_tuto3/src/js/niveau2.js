d09dae36c6af9a49f3ca22279c2b2065720a3f0e
Noemie PHILIP <noemie.philip@epfedu.fr>
2025-03-12 14:43:48 +0100

    niveau 2 réparé

--- phaser_environnement_vide_tuto3/src/js/niveau2.js
+++ phaser_environnement_vide_tuto3/src/js/niveau2.js
@@ -26,7 +26,7 @@ export default class niveau2 extends Phaser.Scene {
     this.load.image("BG", "src/assets/niveau2/BG.png");
     this.load.image("ours", "src/assets/niveau2/ourspolaire.png");
     this.load.image("tileset_image", "src/assets/victoire_image.png");
-    this.load.image("tileset_bouton","src/assets/bouton.png")
+    this.load.image("bouton","src/assets/bouton.png")
     this.load.tilemapTiledJSON("map2", "src/assets/niveau2/mapBanquise.json");
   }
 
@@ -347,4 +347,5 @@ this.time.delayedCall(1000, () => {
     
 
   }
+  
 }
